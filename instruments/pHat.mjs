0a9241ba46cf232510b0faed48a03278762b708f
acerix <dylan@psilly.com>
2018-11-25 20:31:46 -0500

    less hat

--- instruments/pHat.mjs
+++ instruments/pHat.mjs
@@ -21,8 +21,12 @@ export class pHat {
     // Create Tone.js player
     var source = new Tone.Player(options.audio_url).toMaster()
 
-    // Play on event 16th note
+    // Play on most 16th notes
+    var note = -1
+    var skip_notes = false
     Tone.Transport.scheduleRepeat(function(time){
+      note++
+      if (skip_notes && note % 2 === 1) return
       source.start()
     }, '16n')
 
@@ -32,7 +36,8 @@ export class pHat {
     Tone.Transport.scheduleRepeat(function(time){
       measure++
       if (measure % 4 !== 2) return
-      if (Math.random() > 0.25) {
+      skip_notes = Math.random() > 0.5
+      if (Math.random() > 0.6) {
         source.volume.value = 0
       }
       else {
